48f286cad3d2d80fa78ea67a7f73cf9d446a8de3
bogdanpatroi <bujica.bogdan@gmail.com>
2020-07-26 21:00:07 +0300

    improving the Welcome Screen

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
 //import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { View } from "react-native";
+import { View, Text } from "react-native";
+
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
+import AppText from "./app/components/AppText";
+import AppButton from "./app/components/AppButton";
 
 export default function App() {
-  return <ViewImageScreen />;
+  return <WelcomeScreen />;
 }
