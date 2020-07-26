54613e58b1ddd4fdfb82f00685e9ea411ec3f388
bogdanpatroi <bujica.bogdan@gmail.com>
2020-07-26 23:23:22 +0300

    using FlatList in MessagesScreen, Separators

---
+++ app/screens/MessagesScreen.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { StyleSheet, FlatList, View } from "react-native";
+
+import ListItem from "../components/ListItem";
+import Screen from "../components/Screen";
+import colors from "../config/colors";
+import ListItemSeparator from "../components/ListItemSeparator";
+
+const messages = [
+  {
+    id: 1,
+    title: "T1 title",
+    description: "D1 description",
+    image: require("../assets/bogdan.jpg"),
+  },
+  {
+    id: 2,
+    title: "T2 title",
+    description: "D2 description",
+    image: require("../assets/bogdan.jpg"),
+  },
+];
+
+export default function MessagesScreen() {
+  return (
+    <Screen>
+      <FlatList
+        data={messages}
+        keyExtractor={(messages) => messages.id.toString()}
+        renderItem={({ item }) => (
+          <ListItem
+            title={item.title}
+            subTitle={item.description}
+            image={item.image}
+          />
+        )}
+        ItemSeparatorComponent={ListItemSeparator}
+      />
+    </Screen>
+  );
+}
+
+const styles = StyleSheet.create({});
