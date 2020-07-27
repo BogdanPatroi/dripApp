3c9a725ae4de26772ee0346df4de6f1922f73e58
bogdanpatroi <bujica.bogdan@gmail.com>
2020-07-27 20:58:00 +0300

    building RegisterScreen

---
+++ app/screens/RegisterScreen.js
@@ -0,0 +1,68 @@
+import React from "react";
+import { StyleSheet, Image } from "react-native";
+import * as Yup from "yup";
+
+import Screen from "../components/Screen";
+import { AppForm, AppFormField, SubmitButton } from "../components/forms";
+
+const validationSchema = Yup.object().shape({
+  userName: Yup.string().required().min(2).max(36).label("Name"),
+  email: Yup.string().required().email().label("Email"),
+  password: Yup.string().required().min(4).label("Password"),
+});
+
+export default function LoginScreen() {
+  return (
+    <Screen style={styles.container}>
+      {/* <Image style={styles.logo} source={require("../assets/logo-red.png")} /> */}
+      <AppForm
+        initialValues={{ userName: "", email: "", password: "" }}
+        onSubmit={(values) => console.log(values)}
+        validationSchema={validationSchema}
+      >
+        <AppFormField
+          maxLength={37}
+          name="userName"
+          autoCapitalize="words"
+          autoCorrect={true}
+          icon="account"
+          placeholder="Name"
+          textContentType="name"
+        />
+        <AppFormField
+          name="email"
+          autoCapitalize="none"
+          autoCorrect={false}
+          icon="email"
+          keyboardType="email-address"
+          placeholder="Email"
+          textContentType="emailAddress"
+        />
+
+        <AppFormField
+          name="password"
+          autoCapitalize="none"
+          autoCorrect={false}
+          icon="lock"
+          placeholder="Passwords"
+          secureTextEntry
+          textContentType="password"
+        />
+        <SubmitButton title="Login" />
+      </AppForm>
+    </Screen>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+  },
+  logo: {
+    width: 80,
+    height: 80,
+    alignSelf: "center",
+    marginTop: 50,
+    marginBottom: 20,
+  },
+});
