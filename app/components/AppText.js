bc667ce284e9db2c36297507b96460b09b7152d4
bogdanpatroi <bujica.bogdan@gmail.com>
2020-07-27 13:55:13 +0300

    refactoring default styles

--- app/components/AppText.js
+++ app/components/AppText.js
@@ -1,13 +1,9 @@
 import React from "react";
-import { StyleSheet, Text, Platform } from "react-native";
+import { StyleSheet, Text } from "react-native";
 
+import defaultStyles from "../config/styles";
 export default function AppText({ children, style }) {
-  return <Text style={[styles.text, style]}> {children} </Text>;
+  return <Text style={[defaultStyles.text, style]}> {children} </Text>;
 }
 
-const styles = StyleSheet.create({
-  text: {
-    fontSize: 18,
-    fontFamily: Platform.OS === "android" ? "Roboto" : "Avenir",
-  },
-});
+const styles = StyleSheet.create({});
