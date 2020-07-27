227fc1afb1889ce3e53baaca291c92b8d8e2661d
bogdanpatroi <bujica.bogdan@gmail.com>
2020-07-27 19:51:57 +0300

    fixing some stuff

--- app/screens/ListingEditScreen.js
+++ app/screens/ListingEditScreen.js
@@ -11,7 +11,7 @@ import {
 import Screen from "../components/Screen";
 
 const validationSchema = Yup.object().shape({
-  title: Yup.string().required().min(1).label("Title"),
+  title: Yup.string().required().min(3).max(124).label("Title"),
   price: Yup.number().required().min(1).max(10000).label("Price"),
   description: Yup.string().label("Description"),
   category: Yup.object().required().nullable().label("Category"),
@@ -36,7 +36,7 @@ export default function ListingEditScreen() {
         onSubmit={(values) => console.log(values)}
         validationSchema={validationSchema}
       >
-        <FormField maxLength={255} name="title" placeholder="Title" />
+        <FormField maxLength={124} multiline name="title" placeholder="Title" />
         <FormField
           keyboardType="numeric"
           maxLength={8}
