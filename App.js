1c625efd8ce944799d823f0089a17d491fe954f7
bogdanpatroi <bujica.bogdan@gmail.com>
2020-07-28 20:04:45 +0300

    playng with ImagePicker, ask for permissions, permission module and accesing the library

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 //import { StatusBar } from "expo-status-bar";
-import React, { useState } from "react";
-import { View, Text } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, Text, Image, Button } from "react-native";
+import * as ImagePicker from "expo-image-picker";
 
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
@@ -20,6 +21,31 @@ import LoginScreen from "./app/screens/LoginScreen";
 import ListingEditScreen from "./app/screens/ListingEditScreen";
 import RegisterScreen from "./app/screens/RegisterScreen";
 
+const requestPermission = async () => {
+  const { granted } = await ImagePicker.requestCameraRollPermissionsAsync();
+  if (!granted) alert("You need to enable permission to access the library");
+};
+
 export default function App() {
-  return <ListingEditScreen />;
+  const [imageUri, setImageUri] = useState();
+
+  useEffect(() => {
+    requestPermission();
+  }, []);
+
+  const selectImage = async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync();
+      if (!result.cancelled) setImageUri(result.uri);
+    } catch (error) {
+      console.log("Error reading an image", error);
+    }
+  };
+
+  return (
+    <Screen>
+      <Button title="Select Image" onPress={selectImage} />
+      <Image source={{ uri: imageUri }} style={{ width: 200, height: 200 }} />
+    </Screen>
+  );
 }
