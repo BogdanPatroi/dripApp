fbe223c0919e5b0b6983bd626595a1bf899073b7
bogdanpatroi <bujica.bogdan@gmail.com>
2020-08-08 23:07:23 +0300

    handling errors

--- app/screens/ListingScreen.js
+++ app/screens/ListingScreen.js
@@ -6,12 +6,18 @@ import Card from "../components/Card";
 import routes from "../navigation/routes";
 import colors from "../config/colors";
 import listingsApi from "../api/listings";
+import AppText from "../components/AppText";
+import AppButton from "../components/AppButton";
 
 export default function ListingScreen({ navigation }) {
   const [listings, setListings] = useState([]);
+  const [hasError, setHasError] = useState(false);
 
   const loadListings = async () => {
     const response = await listingsApi.getListings();
+    if (!response.ok) return setHasError(true);
+
+    setHasError(false);
     setListings(response.data);
   };
   useEffect(() => {
@@ -20,6 +26,14 @@ export default function ListingScreen({ navigation }) {
 
   return (
     <Screen style={styles.screen}>
+      {hasError && (
+        <>
+          <AppText style={styles.hasErrorScreen}>
+            Couldn't retrieve the listings.
+          </AppText>
+          <AppButton title="Retry" onPress={loadListings} />
+        </>
+      )}
       <FlatList
         data={listings}
         keyExtractor={(listing) => listing.id.toString()}
@@ -40,5 +54,8 @@ const styles = StyleSheet.create({
   screen: {
     padding: 17,
     backgroundColor: colors.light,
+  },
+  hasErrorScreen: {
+    alignSelf: "center",
   },
 });
