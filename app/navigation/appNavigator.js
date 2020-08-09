d735c51c4b9bad705ea1f7d4e4e0948d1e023630
bogdanpatroi <bujica.bogdan@gmail.com>
2020-08-09 23:28:17 +0300

    storing the push notification token

--- app/navigation/appNavigator.js
+++ app/navigation/appNavigator.js
@@ -9,6 +9,7 @@ import FeedNavigator from "./FeedNavigator";
 import AccountNavigator from "./AccountNavigator";
 import routes from "./routes";
 import NewListingButton from "./NewListingButton";
+import expoPushTokensApi from "../api/expoPushTokens";
 
 const Tab = createBottomTabNavigator();
 
@@ -23,7 +24,7 @@ const AppNavigator = () => {
       if (!permission.granted) return;
 
       const token = await Notifications.getExpoPushTokenAsync();
-      console.log(token);
+      expoPushTokensApi.register(token);
     } catch (error) {
       console.log("Error getting a push token", error);
     }
